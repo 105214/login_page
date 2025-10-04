f73c307e1c6cd29594a02e28aa88b00a48975131
nithin <nithinsukumar200@gmail.com>
2025-10-04 22:47:38 +0530

    login page done

--- src/login.jsx
+++ src/login.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import eye from './image/eye.png'
+import closedeye from './image/closedeye.png'
 import './login.css'
 const Login = () => {
+
+  const [passwordVisible,setPasswordVisible]=useState(false)
+  const [email,setEmail]=useState("")
+
+  const togglePassword=()=>{
+setPasswordVisible(!passwordVisible)
+  }
+
+  
   return (
     <div>
       <div className="login-box">
         <div className="bg-layer"></div>
         <div className="login-form">
-
-      <input type="email" placeholder='enter your email'/>
-      <input type="password" placeholder='enter your password' />
-      <button>Login</button>
+       <h2 className="login-heading">Login</h2>
+      <input type="email" value={email} placeholder='enter your email' onChange={(e)=>setEmail(e.target.value)}  />
+      <input type={passwordVisible ? 'text' : 'password'} placeholder='enter your password' />
+      <img src={passwordVisible? eye : closedeye } alt='visibility' className='eye' onClick={togglePassword} />
+    
+      {email && <button className='submit-btn' >Login</button>}
         </div>
       </div>
      
